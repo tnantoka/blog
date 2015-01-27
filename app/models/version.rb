1d5b873ed134fa9c3e5f0d0c9dcc0c9e0aabf07d
tnantoka <tnantoka@bornneet.com>
2015-01-27 23:23:41 +0900

    Fix versions

--- app/models/version.rb
+++ app/models/version.rb
@@ -49,7 +49,7 @@ class Version < PaperTrail::Version
 
   private
     def _diff
-      before = reify.previous_version.try(:content).to_s
+      before = (persisted? ? reify.previous_version : reify.versions.last.try(:reify)).try(:content).to_s
       after = reify.content.to_s
       diff = Diffy::Diff.new(before, after, include_plus_and_minus_in_html: true)
       diff.to_s.present? ? diff.to_s(:html) : nil
