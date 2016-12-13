090f8ea5dcb57005dc113e3eba35eda660359312
tnantoka <tnantoka@bornneet.com>
2016-12-13 22:08:35 +0900

    Update paper_trail

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ require 'capybara/rspec'
 #require 'capybara/poltergeist'
 require 'webmock/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'paper_trail/frameworks/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
